e5db99d5260d7584c20fec6a7ee8d7807cbb8086
Halfiies <morriswillis@outlook.com>
2021-09-17 10:06:52 +0100

    written the quick brown fox jumps over the lazy dog in morse code for tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tddmorsecode
+a project to learn more about tdd by creating an english-morse code translator.
+file structure initially created using bashscript
---
+++ js/functions.test.js
@@ -0,0 +1,11 @@
+// function must return morse code from letter input
+
+// function must return a . when the letter e is inputted
+
+// each letters morse representation should be followed by 3 empty spaces
+
+// each space should return and addintional 4 empty spaces
+
+// function should accept a string of english characters and return a string of morse characters
+// "the quick brown fox jumped over the lazy dog" 
+// "-   ....   .       --.-   ..-   ..   -.-.   -.-       -...   .-.   ---   .--   -.   ..-.   ---   -..-       .---   ..-   --   .--.   ...       ---   ...-   .   .-.       -   ....   .       .-..   .-   --..   -.--       -..   ---   --.   ..."
